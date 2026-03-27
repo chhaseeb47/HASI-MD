7d1797944e742535216d9e170b6ee59fb25eaa0a
HASI X MAFIA <mhmodsyt517@gmail.com>
2026-03-27 20:30:55 +0500

    Change reaction emoji from 💅 to 📃

--- plugins/list-cmd.js
+++ plugins/list-cmd.js
@@ -6,7 +6,7 @@ cmd({
     alias: ["listcmd","commands"],
     desc: "menu the bot",
     category: "menu",
-    react: "💅",
+    react: "📃",
     filename: __filename
 }, 
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
